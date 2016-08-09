86407f8de22fb545c101a45e8aa908108a1f45d0
郑露 <zhenglu@zhengludeMacBook-Pro.local>
2016-08-09 16:43:12 +0800

    Initial commit

---
+++ src/main/java/Boot.java
@@ -0,0 +1,23 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author buer
+ * @since 16/8/9
+ */
+
+@RestController
+@EnableAutoConfiguration
+public class Boot {
+
+	@RequestMapping("/hello")
+	String home(){
+		return "Hello world";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot.class,args);
+	}
+}
