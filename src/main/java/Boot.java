e78976b48ff707f75004cb43df79cd46997d4bb4
郑露 <zhenglu@zhengludeMacBook-Pro.local>
2016-08-09 18:10:37 +0800

    添加修改端口支持，实现EmbeddedServletContainerCustomizer接口

--- src/main/java/Boot.java
+++ src/main/java/Boot.java
@@ -1,5 +1,7 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
-public class Boot {
+public class Boot  implements EmbeddedServletContainerCustomizer{
 
 	@RequestMapping("/hello")
 	String home(){
-		return "Hello world";
+		return "Hello world1111";
 	}
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Boot.class,args);
+	}
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		container.setPort(8081);
+
 	}
 }
